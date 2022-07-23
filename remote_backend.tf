3ce2f07efa45a8e7df1b89d8f476e64188a49ccf
dso <dnadae@gmail.com>
2022-07-23 11:35:12 +0000

    Added the first tag

--- remote_backend.tf
+++ remote_backend.tf
@@ -3,8 +3,6 @@ terraform {
     hostname = "app.terraform.io"
 <<<<<<< HEAD
     organization = "hashids-train"
-=======
-    organization = "AWS-dso-test"
 >>>>>>> 8564c0344597f79c2c6e9a2bcffc6be71dfe85ed
     workspaces {
       name = "hashicat-aws"
